9aa8a3f7ea920f322e4242b6f2b3550a1c9afefc
Kunal Mandave <mandavekunal@gmail.com>
2024-01-13 15:19:09 +0530

    feat: Implement video filtering, sorting, and pagination

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,11 +9,74 @@ import {
 } from "../utils/cloudinaryFileUpload.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  const {
+    page = 1,
+    limit = 10,
+    query = " ",
+    sortBy,
+    sortType,
+    userId,
+  } = req.query;
   // TODO: get all videos based on query, sort, pagination
+
+  let options = {
+    page: isNaN(page) ? 1 : Number(page),
+    limit: isNaN(limit) ? 10 : Number(limit),
+  };
+
+  if (page <= 0) {
+    page = 1;
+  }
+  if (limit <= 0) {
+    page = 10;
+  }
+
+  const matchStage = {};
+
+  if (userId && isValidObjectId(userId)) {
+    matchStage.owner = {
+      owner: new mongoose.Types.ObjectId(userId),
+    };
+  }
+  if (query) {
+    matchStage.$match = {
+      $or: [
+        {
+          title: {
+            $regex: query,
+            $options: "i", // IMP: This 'i' is for case insensitive
+          },
+        },
+        {
+          description: {
+            $regex: query,
+            $options: "i",
+          },
+        },
+      ],
+    };
+  } else {
+    matchStage.$match = {};
+  }
+
+  const sortOptions = {};
+
+  if (sortBy && sortType) {
+    sortOptions[sortType] = sortBy === "desc" ? -1 : 1;
+  }
+
+  const videos = await Video.aggregatePaginate(matchStage, {
+    page: parseInt(page),
+    limit: parseInt(limit),
+    sort: sortOptions,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, videos, "Videos retrieved successfully..."));
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -85,20 +148,90 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+
+  if (!videoId) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  if (isValidObjectId(videoId)) {
+    throw new ApiError(404, "Video id is not correct");
+  }
+
+  const video = await Video.findById(videoId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetch successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+
+  const { title, description } = req.body;
+
+  if (!videoId) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title,
+        description,
+      },
+    },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video details update successfully"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: delete video
+
+  if (!videoId) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const deletedVideo = await Video.findByIdAndDelete(videoId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedVideo, "Video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+  if (!videoId || isValidObjectId(videoId)) {
+    throw new ApiError(404, "Video not found ");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found ");
+  }
+
+  video.isPublished = !video.isPublished;
+
+  await video.save();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        `Publish status toggled successfully. New status: ${
+          video.isPublished ? "Published" : "Unpublished"
+        }`,
+        video
+      )
+    );
 });
 
 export {
@@ -109,16 +242,3 @@ export {
   deleteVideo,
   togglePublishStatus,
 };
-
-/**
- * TODO: Work left
- * - Edit thumbnail and Video description or title
- * - Add like feature
- *
- * @problems
- * - How you edit that particular video data you can use owner detail but owner have more than one video data
- *
- * @solutions
- * - 1) you can use video _id but how an you find its owner is logged in or not you can check its id but you can ...
- * - 2) it necessary to add the middleware as auth.middleware for video to get the data of video
- */
